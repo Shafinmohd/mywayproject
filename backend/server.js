a2db1a19396cceb114935a19141d9374f462a246
Shafinmohd <Shafinmd808@gmail.com>
2023-10-19 18:15:36 +0530

    First commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { chats } = require("./data/data.js");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
